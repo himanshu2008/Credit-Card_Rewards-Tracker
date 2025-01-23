dbd06c6cf559a28c3e71b243c03e99252792a8a9
HimanshuChopra <himanshuchopra2008@outlook.com>
2025-01-23 21:43:03 +0530

    Initial Commit

---
+++ credit_card_rewards/src/main/java/com/project2008/credit_card_rewards/CreditCardRewardsApplication.java
@@ -0,0 +1,13 @@
+package com.project2008.credit_card_rewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardRewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardRewardsApplication.class, args);
+	}
+
+}
---
+++ credit_card_rewards/src/test/java/com/project2008/credit_card_rewards/CreditCardRewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project2008.credit_card_rewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditCardRewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
